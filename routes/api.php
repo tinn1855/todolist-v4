0089549efa1ac5adeff41e215e0bda500df90c0d
tinnguyen <tinn1855@gmail.com>
2025-06-02 18:51:53 +0700

    feat: code feature login/logout

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/todos', [TodoController::class, 'index']);
 Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
