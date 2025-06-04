6c93875ef6e38681ddf1111c8ce7618480be7864
tinnguyen <tinn1855@gmail.com>
2025-06-04 17:43:15 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,12 @@ use App\Http\Controllers\AuthController;
 
 
 Route::get('/users', [UserController::class, 'index']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/todos', [TodoController::class, 'index']);
-
 });
 
-
-Route::group(['prefix' => 'api', 'as' => 'api.', 'middleware' => 'validate_api_token'], function () {
-    Route::post('api/checkLogin', 'ApiController@getLoginStatus')->name('check_login_status');
-});
 
 
