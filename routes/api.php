7919d49cdd87d8cf2589765a214ffdd96c38e4fe
tinnguyen <tinn1855@gmail.com>
2025-06-04 16:27:03 +0700

    fix get todos

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\AuthController;
 
 
 Route::get('/users', [UserController::class, 'index']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/todos', [TodoController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::get('/todos', [TodoController::class, 'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
